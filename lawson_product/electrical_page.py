36c8ab87940eef783b9a5c51bd0e703f98ccd73a
pawanloki <kumarpavan79015@gmail.com>
2024-12-13 20:20:35 +0530

    initialise the browser by using relative chromepath

--- lawson_product/electrical_page.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-import time
-
-class Electrical:
-    def __init__(self, driver):
-        self.driver = driver
-    def navigate_to_elecrical(self):
-        self.driver.find_element(By.XPATH, "//a[@id='ip8zy-4-88-3-69']").click()
-        time.sleep(2)
