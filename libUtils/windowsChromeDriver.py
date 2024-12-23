4410667da56a17b2cbad3d68f41668a18b36df40
satish <satishmohan69@gmail.com>
2024-12-23 17:09:06 +0530

    auto download windows chrome driver

--- libUtils/windowsChromeDriver.py
+++ libUtils/windowsChromeDriver.py
@@ -4,14 +4,14 @@ import os
 import zipfile
 import platform
 
-class BrowserManager:
 
+class BrowserManager:
     def __init__(self):
-        self.driver_path = ".//dependencies//chrome-driver"
+        self.driver_path = "..//dependencies//chrome-driver"
         # self.base_url = f"https://storage.googleapis.com/chrome-for-testing-public/{chrome_version}/win64/chromedriver-win64.zip"
         self.reg_path = r'reg query "HKEY_CURRENT_USER\Software\Google\Chrome\BLBeacon" /v version'
 
-    def get_chrome_version(self):   #Get the Chrome browser version installed on Windows.
+    def get_chrome_version(self):  #Get the Chrome browser version installed on Windows.
         try:
             output = subprocess.check_output(self.reg_path, shell=True, encoding='utf-8')
             # Extract version from the command output
@@ -19,10 +19,10 @@ class BrowserManager:
             print(f"Chrome Version: {version}")
             return version
         except Exception as e:
-            print( f"Failed to read chrome browser version from path: {self.reg_path}")
+            print(f"Failed to read chrome browser version from path: {self.reg_path}")
             raise Exception(f"Failed to get chrome browser version. Error: {e}")
 
-    def get_driver_url(self,chrome_version):
+    def get_driver_url(self, chrome_version):
         try:
             architecture = 'win64' if platform.architecture()[0] == '64bit' else 'win32'
             print(architecture)
@@ -34,6 +34,8 @@ class BrowserManager:
                 raise Exception(f"Failed to get ChromeDriver for Chrome {chrome_version}.")
             return download_url
         except Exception as e:
+            print(f"status code and response HTTP status code: response.status_code")
+
             raise Exception(f"Error getting ChromeDriver download URL: {e}")
 
     def download_zip_file(self, download_url):
